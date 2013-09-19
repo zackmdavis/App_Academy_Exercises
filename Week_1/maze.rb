a13807c54a9f7112fd0cd2024b30eeea4063bf0c
Zack M. Davis <code@zackmdavis.net>
2013-09-18 18:03:02 -0700

    A* tinkering with Jen Hamon

--- Week_1/maze.rb
+++ Week_1/maze.rb
@@ -2,6 +2,24 @@
 file = File.open(ARGV[0], "r")
 
 
+class Node
+  :attr_reader f, g
+  :attr_accessor coord, parent, h
+
+  def initialize(coord, parent, finish)
+    @coord = coord
+    @parent = parent
+    @g = parent.g + 1
+    @h = (finish[0]-here[0]).abs + (finish[1]-here[1]).abs
+    @f = @g + @h
+  end
+
+  def g=(new_g)
+    @g = new_g
+    @f = @g + @h
+  end
+end
+
 class Maze
   def initialize(specs)
     @maze = []
@@ -10,10 +28,15 @@ class Maze
     end
     @maze.each_index do |i|
       @maze[i].each_index do |j|
-        if @maze[i][j] == 'S'
-          @start = [i,j]
-        elsif @maze[i][j] =='F'
-          @finish = [i,j]
+        if @maze[i][j] =='F'
+          @finish = Node.new([i,j], nil, [i,j])
+        end
+      end
+    end
+    @maze.each_index do |i|
+      @maze[i].each_index do |j|
+        if @maze[i][j] =='S'
+          @start = Node.new([i,j], nil, @finish.coord)
         end
       end
     end
@@ -30,19 +53,54 @@ class Maze
     end
   end
 
+#  {[i,j] => {:parent => [i,j], :g => score}
 
-  def search
-    @path = []
-    closed = []
-    open = []
+def adjacents(coord)
+  adjacents = []
+  (-1..1).each do |i|
+    (-1..1).each do |j|
+      unless i == 0 and j == 0
+        adjacents << [coord[0]+i, coord[1]+j]
+      end
+    end
   end
+  adjacents
+end
 
-  def solve
-    @solved = @maze.map(&:dup)
+def find_in(set, coord)
+  set.find{|node| node.coord == coord}
+end
+
+def search
+  closed = []
+  open = [@start]
+  while !open.empty?
+    open.sort_by!{|node| node.f}
+    current = open.shift
+    closed << current
+    adjacents(current).each do |adj|
+      if adj == @finish
+        find_path(closed, current)
+      elsif find_in(close, adj)
+        puts "already in closed"
+      elsif find_in(open, adj)
+        adj_node = find_in(open, adj)
+        if adj_node.g > current.g + 1
+          adj_node.parent = current
+          adj_node.g = current.g + 1 # also recomputes adj_node.f as side effect
+      else
+        if @maze[adj[0]][adj[1]] == ' '
+          open << Node.new(adj, current, @finish)
+        end
+      end
+    end
+  end
+end
+
+def find_path(nodes, penultimate)
+  path = [penultimate]
+  while path.last.parent != nil
     # TODO
-  end
-
-
 end
 
 
