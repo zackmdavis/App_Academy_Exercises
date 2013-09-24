4acc4ea389f989734b374517efa841a28cedb38d
Zack M. Davis <code@zackmdavis.net>
2013-09-24 15:23:36 -0700

    wrote game

--- Week_2/chess.rb
+++ Week_2/chess.rb
@@ -14,44 +14,166 @@ end
 
 class ChessGame
 
+  def initialize
+    setup
+    play
+  end
+
+  def setup
+    @board = ChessBoard.new
+    @player1 = HumanPlayer.new(@board, :white)
+    @player2 = HumanPlayer.new(@board, :black)
+  end
+
+  def play
+    white_playing = true
+    playing = @player1
+    until @board.checkmated?(:white) or @board.checkmated?(:black)
+      while true
+        @board.show
+        playing.select_piece
+        @board.show
+        break if playing.select_move
+      end
+
+      case white_playing
+      when true
+        playing = @player2
+      when false
+        playing = @player1
+      end
+      white_playing = !white_playing
+    end
+
+  end
+
+end
+
+class HumanPlayer
+
+  def initialize(board, color)
+    @board = board
+    @color = color
+  end
+
+  def get_char
+    # Thanks to http://stackoverflow.com/questions/8142901/ruby-stdin-getc-does-not-read-char-on-reception
+    begin
+      system("stty raw -echo")
+      str = STDIN.getc
+    ensure
+      system("stty -raw echo")
+    end
+    str.chr
+  end
+
+  def get_command
+    while true
+      command = get_char
+      case command
+      when 'z'
+        return true
+      when 'x'
+        return false
+      when 'q'
+        abort("Quit")
+      else
+        move_cursor(command)
+      end
+    end
+  end
+
+  def move_cursor(command_key)
+    new_location = @board.cursor_location.dup
+    case command_key
+    when 'i'
+      new_location[0] -= 1
+    when 'j'
+      new_location[1] -= 1
+    when 'k'
+      new_location[0] += 1
+    when 'l'
+      new_location[1] += 1
+    end
+    if ChessBoard.in_bounds?(new_location)
+      @board.cursor_location = new_location
+      @board.show
+    end
+  end
+
+  def select_move
+    return false unless get_command
+    if @board.possible_moves.include?(@board.cursor_location)
+      @board.make_move(@selected_piece, @board.cursor_location)
+      @board.possible_moves = []
+    else
+      return select_move
+    end
+    true
+  end
+
+  def select_piece
+    while true
+      break if get_command
+    end
+    piece = @board.at(@board.cursor_location)
+    if !piece.nil? && piece.color == @color
+      legal_moves = piece.legal_moves
+      @board.possible_moves = legal_moves
+      @selected_piece = piece
+    else
+      select_piece
+    end
+  end
+
 end
 
 
 class ChessBoard
 
-  attr_accessor :board
+  attr_accessor :board, :cursor_location, :possible_moves
 
   def initialize(board = nil)
     if board.nil?
       board = Array.new(8){ Array.new(8) }
-      board[0][0] = Rook.new(self, [0,0], :black)
-      board[0][1] = Knight.new(self, [0,1], :black)
-      board[0][2] = Bishop.new(self, [0,2], :black)
-      board[0][3] = Queen.new(self, [0,3], :black)
-      board[0][4] = King.new(self, [0,4], :black)
-      board[0][5] = Bishop.new(self, [0,5], :black)
-      board[0][6] = Knight.new(self, [0,6], :black)
-      board[0][7] = Rook.new(self, [0,7], :black)
+
+      place_major_pieces(board, :black)
       (0..7).each do |j|
         board[1][j] = Pawn.new(self, [1, j], :black)
       end
       (0..7).each do |j|
         board[6][j] = Pawn.new(self, [6, j], :white)
       end
-      board[7][0] = Rook.new(self, [7,0], :white)
-      board[7][1] = Knight.new(self, [7,1], :white)
-      board[7][2] = Bishop.new(self, [7,2], :white)
-      board[7][3] = Queen.new(self, [7,3], :white)
-      board[7][4] = King.new(self, [7,4], :white)
-      board[7][5] = Bishop.new(self, [7,5], :white)
-      board[7][6] = Knight.new(self, [7,6], :white)
-      board[7][7] = Rook.new(self, [7,7], :white)
+      place_major_pieces(board, :white)
     end
     @board = board
+    @cursor_location = [0,0]
+    @possible_moves = []
   end
 
-  def in_bounds?(position)
-    positions.all? { |coordinate| coordinate >= 0 and coordinate < 8 }
+  def place_major_pieces(board, color)
+    if color == :black
+      row = 0
+    elsif color == :white
+      row = 7
+    end
+
+    pieces = [Proc.new{ | position | Rook.new(self, position, color) },
+              Proc.new{ | position | Knight.new(self, position, color) },
+              Proc.new{ | position | Bishop.new(self, position, color) },
+              Proc.new{ | position | Queen.new(self, position, color) },
+              Proc.new{ | position | King.new(self, position, color) },
+              Proc.new{ | position | Bishop.new(self, position, color) },
+              Proc.new{ | position | Knight.new(self, position, color) },
+              Proc.new{ | position | Rook.new(self, position, color) }]
+
+    pieces.each_with_index do |piece, index|
+      board[row][index] = piece.call([row, index])
+    end
+  end
+
+  def ChessBoard.in_bounds?(position)
+    position.all? { |coordinate| coordinate >= 0 and coordinate < 8 }
   end
 
   def at(position)
@@ -63,25 +185,44 @@ class ChessBoard
   end
 
   def make_move(piece, position)
-    piece.position = nil
+    @board[piece.position[0]][piece.position[1]] = nil
     @board[position[0]][position[1]] = piece
     piece.position = position
+    self
   end
 
   def in_check?(color)
-    false
+    false # placeholder
+  end
+
+  def checkmated?(color)
+    false # placeholder
+  end
+
+  def ChessBoard.square_colored?(position)
+    if (position[0]+position[1]) % 2 == 1
+      return true
+    else
+      return false
+    end
   end
 
   def show
-    print "  a b c d e f g h\n"
+    print " a b c d e f g h\n"
+    square_color = {true => :black, false => :red}
 
     @board.each_with_index do |row, row_index|
       print (8 - row_index).to_s
-      row.each do |square|
-        if square.nil?
-          print "  "
+      row.each_with_index do |square, col_index|
+        if [row_index, col_index] == @cursor_location
+          print "\u2B21 ".colorize(:color => :cyan, :background => square_color[ChessBoard.square_colored?([row_index, col_index])])
+        elsif possible_moves.include?([row_index, col_index])
+          print "X ".colorize(:color => :cyan, :background => square_color[ChessBoard.square_colored?([row_index, col_index])])
+        elsif square.nil?
+          print "  ".colorize(:background => square_color[ChessBoard.square_colored?([row_index, col_index])])
         else
-          print "#{square.icon} "
+          print square.icon.colorize(:background => square_color[ChessBoard.square_colored?([row_index, col_index])])
+          print " ".colorize(:background => square_color[ChessBoard.square_colored?([row_index, col_index])])
         end
       end
       print "\n"
@@ -100,7 +241,16 @@ class ChessPiece
     @color = color
   end
 
-  def move(new_position)
+  def move_from_offset(offset)
+    [].tap do |candidate|
+      @position.each_with_index do |coordinate, i|
+        candidate.push(coordinate + offset[i])
+      end
+    end
+  end
+
+  def reject_check_moves(moves)
+    moves.reject { |move| @board.dup.make_move(self, move).in_check?(@color) }
   end
 
 end
@@ -109,16 +259,11 @@ class SlidingPiece < ChessPiece
 
   def legal_moves
     moves = []
-    @@directions.each do |direction|
+    directions.each do |direction|
       multiplier = 1
       while true
         offset = direction.map { |coordinate| multiplier * coordinate }
-        candidate_move = [].tap do |candidate|
-                           @position.each_with_index do |coordinate, i|
-                             candidate.push(coordinate + offset[i])
-                           end
-                         end
-
+        candidate_move = move_from_offset(offset)
         if !ChessBoard.in_bounds?(candidate_move)
           break
         elsif !@board.at(candidate_move).nil?
@@ -134,8 +279,7 @@ class SlidingPiece < ChessPiece
         multiplier += 1
       end
     end
-
-    moves.reject { |move| @board.dup.make_move(self, move).in_check?(@color) }
+    reject_check_moves(moves)
   end
 end
 
@@ -143,12 +287,8 @@ class SteppingPiece < ChessPiece
 
   def legal_moves
     moves = []
-    @@offsets.each do |offset|
-      candidate_move = [].tap do |candidate|
-                           @position.each_with_index do |coordinate, i|
-                             candidate.push(coordinate + offset[i])
-                           end
-                         end
+    offsets.each do |offset|
+      candidate_move = move_from_offset(offset)
       if ChessBoard.in_bounds?(candidate_move)
         if @board.at(candidate_move).nil?
           moves.push(candidate_move)
@@ -157,7 +297,7 @@ class SteppingPiece < ChessPiece
         end
       end
     end
-    moves.reject { |move| @board.dup.make_move(self, move).in_check?(@color) }
+    reject_check_moves(moves)
   end
 
 end
@@ -166,77 +306,103 @@ class Pawn < ChessPiece
   def initialize(board, position, color)
     super(board, position, color)
     if color == :white
-      @icon = "\u2659"
+      @icon = "\u2659".colorize(:yellow)
+      @forward = -1
     elsif color == :black
-      @icon = "\u265F".colorize(:red)
+      @icon = "\u265F".colorize(:green)
+      @forward = 1
     end
+  end
+
+  def legal_moves
+    moves = []
+    if @board.at([@position[0]+@forward, @position[1]]).nil?
+      moves.push([@position[0]+@forward, @position[1]])
+    end
+    [-1, 1].each do |j|
+      piece = @board.at([@position[0]+@forward, @position[1]+j])
+      if !piece.nil? and piece.color != @color
+        moves.push([@position[0]+@forward, @position[1]+j])
+      end
+    end
+    moves = reject_check_moves(moves)
+    moves.select { |move| ChessBoard.in_bounds?(move) }
   end
 end
 
 class King < SteppingPiece
-  @@offsets = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+  def offsets
+    [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+  end
 
   def initialize(board, position, color)
     super(board, position, color)
     if color == :white
-      @icon = "\u2654"
+      @icon = "\u2654".colorize(:yellow)
     elsif color == :black
-      @icon = "\u265A".colorize(:red)
+      @icon = "\u265A".colorize(:green)
     end
   end
 end
 
 class Queen < SlidingPiece
-  @@directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+  def directions
+    [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+  end
 
   def initialize(board, position, color)
     super(board, position, color)
     if color == :white
-      @icon = "\u2655"
+      @icon = "\u2655".colorize(:yellow)
     elsif color == :black
-      @icon = "\u265B".colorize(:red)
+      @icon = "\u265B".colorize(:green)
     end
   end
 end
 
 class Rook < SlidingPiece
-  @@directions = [[-1, 0], [0, -1], [0, 1], [1, 0]]
+  def directions
+    [[-1, 0], [0, -1], [0, 1], [1, 0]]
+  end
 
   def initialize(board, position, color)
     super(board, position, color)
     if color == :white
-      @icon = "\u2656"
+      @icon = "\u2656".colorize(:yellow)
     elsif color == :black
-      @icon = "\u265C".colorize(:red)
+      @icon = "\u265C".colorize(:green)
     end
   end
 end
 
 class Bishop < SlidingPiece
-  @@directions = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
+  def directions
+    [[-1, -1], [-1, 1], [1, -1], [1, 1]]
+  end
 
   def initialize(board, position, color)
     super(board, position, color)
     if color == :white
-      @icon = "\u2657"
+      @icon = "\u2657".colorize(:yellow)
     elsif color == :black
-      @icon = "\u265D".colorize(:red)
+      @icon = "\u265D".colorize(:green)
     end
   end
 end
 
 class Knight < SteppingPiece
-  @@offsets = [[1,2], [2,1], [-1,2], [-2,1], [1,-2], [2,-1], [-1,-2], [-2,-1]]
+  def offsets
+    [[1,2], [2,1], [-1,2], [-2,1], [1,-2], [2,-1], [-1,-2], [-2,-1]]
+  end
 
   def initialize(board, position, color)
     super(board, position, color)
     if color == :white
-      @icon = "\u2658"
+      @icon = "\u2658".colorize(:yellow)
     elsif color == :black
-      @icon = "\u265E".colorize(:red)
+      @icon = "\u265E".colorize(:green)
     end
   end
 end
 
-board = ChessBoard.new
-board.show
+game = ChessGame.new
