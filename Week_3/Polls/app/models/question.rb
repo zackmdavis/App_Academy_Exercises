41cb00ecceee644e10d92ffd60dc874d99f58b69
Zack M. Davis <code@zackmdavis.net>
2013-10-03 15:05:49 -0700

    builds validations

--- Week_3/Polls/app/models/question.rb
+++ Week_3/Polls/app/models/question.rb
@@ -6,4 +6,14 @@ class Question < ActiveRecord::Base
   belongs_to :poll, :primary_key => :id, :foreign_key => :poll_id, :class_name => "Poll"
 
   has_many :answer_choices, :primary_key => :id, :foreign_key => :question_id, :class_name => "AnswerChoice"
+
+  def results
+    choices = answer_choices
+    # maybe rewrite late
+    counts = Hash.new(0)
+    choices.each do |choice|
+      counts[choice.text] += choice.responses.count
+    end
+    counts
+  end
 end
--- Week_3/Polls/app/models/response.rb
+++ Week_3/Polls/app/models/response.rb
@@ -3,8 +3,49 @@ class Response < ActiveRecord::Base
 
   validates :user_id, :presence => true
   validates :answer_choice_id, :presence => true
+  validate :respondent_has_not_already_answered_question
+  validate :poll_author_cannot_answer_own_poll
 
   belongs_to :respondent, :primary_key => :id, :foreign_key => :user_id, :class_name => "User"
 
   belongs_to :answer_choice, :primary_key => :id, :foreign_key => :answer_choice_id, :class_name => "AnswerChoice"
+
+
+  def poll_author_cannot_answer_own_poll
+
+
+  end
+
+  def respondent_has_not_already_answered_question
+    unless existing_responses.empty? or (existing_responses.length == 1 and existing_responses[0] == self.id)
+      errors[:base] << "You can't answer the same question more than once!!"
+    end
+  end
+
+  def existing_responses
+    query  = %Q[
+      SELECT
+        responses.id
+      FROM
+        responses
+      JOIN
+        answer_choices
+      ON
+        responses.answer_choice_id = answer_choices.id
+      WHERE
+        answer_choices.question_id
+        IN (
+          SELECT questions.id
+          FROM
+            questions
+          JOIN
+            answer_choices ON questions.id = answer_choices.question_id
+          WHERE
+            answer_choices.id = ?)
+      AND
+        responses.user_id = ?
+        ]
+    Response.find_by_sql([query, self.answer_choice_id, self.user_id]).map { |obj| obj.id }
+  end
+
 end
