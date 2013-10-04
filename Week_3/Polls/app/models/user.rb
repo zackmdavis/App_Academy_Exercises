3f40c39aa5227615eec99b60fd3dfc9b7ac95293
Zack M. Davis <code@zackmdavis.net>
2013-10-03 18:07:36 -0700

    attempts completed polls for user

--- Week_3/Polls/app/models/user.rb
+++ Week_3/Polls/app/models/user.rb
@@ -10,4 +10,27 @@ class User < ActiveRecord::Base
            :primary_key => :id,
            :foreign_key => :user_id,
            :class_name => "Response"
+
+
+  # BROKEN
+  def completed_poll_ids
+
+    choice_ids = responses.map { |r| r.answer_choice_id }
+
+    polls = Poll.includes(:questions => :answer_choices)
+    completed_ids = []
+    polls.each do |p|
+      completed = true
+      p.questions do |q|
+        if q.answer_choices.map { |c| c.id } & choice_ids == []
+          completed = false
+        end
+      end
+      if completed
+        completed_poll_ids.push(p.id)
+      end
+    end
+    completed_ids
+  end
+
 end
