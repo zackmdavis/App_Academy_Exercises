cba62faea2e1aa3306a251edadf597512e096480
Zack M. Davis <code@zackmdavis.net>
2013-10-08 17:32:15 -0700

    user and status methods with William Ott

--- Week_4/TwitterClient/app/models/status.rb
+++ Week_4/TwitterClient/app/models/status.rb
@@ -1,7 +1,37 @@
+require 'json'
+
 class Status < ActiveRecord::Base
   attr_accessible :body, :twitter_status_id, :twitter_user_id
 
   validates :body, :presence => true
   validates :twitter_user_id, :presence => true
   validates :twitter_status_id, :presence => true, :uniqueness => true
+
+  belongs_to :user, :class_name => "User", :primary_key => :twitter_user_id, :foreign_key => :twitter_user_id
+
+  def self.fetch_statuses_for_user(user)
+    access_token = YAML::load(File.open("lib/saved_token.yaml", 'r'))
+    if !user.statuses.empty?
+      most_recent_id = self.get_most_recent_status(user).twitter_status_id
+      url = "https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=#{user.twitter_user_id}&since_id=#{most_recent_id}"
+      statuses_raw_json = access_token.get(url).body
+      status_hashes = JSON.parse(statuses_raw_json)
+      status_hashes.each do |status_hash|
+        truncated_status_hash = {:body => status_hash["text"], :twitter_status_id => status_hash["id"], :twitter_user_id => user.twitter_user_id }
+        Status.create!(truncated_status_hash)
+      end
+    else
+      url = "https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=#{user.twitter_user_id}&count=9"
+      statuses_raw_json = access_token.get(url).body
+      status_hashes = JSON.parse(statuses_raw_json)
+      status_hashes.each do |status_hash|
+        truncated_status_hash = {:body => status_hash["text"], :twitter_status_id => status_hash["id"], :twitter_user_id => user.twitter_user_id }
+        Status.create!(truncated_status_hash)
+      end
+    end
+  end
+
+  def self.get_most_recent_status(user)
+    user.statuses.max_by { |status| status.twitter_status_id }
+  end
 end
--- Week_4/TwitterClient/app/models/user.rb
+++ Week_4/TwitterClient/app/models/user.rb
@@ -1,11 +1,22 @@
+require 'json'
+
 class User < ActiveRecord::Base
   attr_accessible :screen_name, :twitter_user_id
 
   validates :screen_name, :presence => true
   validates :twitter_user_id, :presence => true, :uniqueness => true
 
-  def self.fetch_by_screen_name
+  has_many :statuses, :class_name => "Status", :primary_key => :twitter_user_id, :foreign_key => :twitter_user_id
 
+  def self.fetch_by_screen_name(screen_name)
+    access_token = YAML::load(File.open("lib/saved_token.yaml", 'r'))
+    user_raw_json = access_token.get("https://api.twitter.com/1.1/users/lookup.json?screen_name=#{screen_name}").body
+    user_data = JSON.parse(user_raw_json).first
+    p self.parse_twitter_params(user_data)
+  end
+
+  def self.parse_twitter_params(user_data)
+    User.create!(:screen_name => user_data["screen_name"], :twitter_user_id => user_data["id"] )
   end
 
 end
