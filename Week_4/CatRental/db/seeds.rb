d15486ebb0da033bd1e2b45c7cc88d7dff08f386
Zack M. Davis <code@zackmdavis.net>
2013-10-10 13:25:54 -0700

    cosmetic improvements

--- Week_4/CatRental/db/seeds.rb
+++ Week_4/CatRental/db/seeds.rb
@@ -1,10 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-c1 = Cat.create!(:name => "Jennifer", :age => 5, :birth_date => "08-Jan-2012", :color => "white", :sex => 'F')
-c2 = Cat.create!(:name => "Spike", :age => 2, :birth_date => "12-Feb-2008", :color => "gray", :sex => 'M')
+c1 = Cat.create!(:name => "Jennifer", :age => 1, :birth_date => "08-Jan-2012", :color => "white", :sex => 'F')
+c2 = Cat.create!(:name => "Spike", :age => 2, :birth_date => "12-Feb-2011", :color => "brown", :sex => 'M')
+c3 = Cat.create!(:name => "Rainbow Dash", :age => 3, :birth_date => "22-Feb-2010", :color => "calico", :sex => 'M')
+c4 = Cat.create!(:name => "Twilight Sparkle", :age => 4, :birth_date => "04-Apr-2009", :color => "gray", :sex => 'F')
+c5 = Cat.create!(:name => "Emperor Norton", :age => 5, :birth_date => "01-Dec-2008", :color => "other", :sex => 'M')
