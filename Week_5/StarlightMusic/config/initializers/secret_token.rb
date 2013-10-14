76b5c2b238fe652542b105b6f083421a135a58d6
Zack M. Davis <code@zackmdavis.net>
2013-10-14 10:10:39 -0700

    begins StarlightMusic application

---
+++ Week_5/StarlightMusic/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StarlightMusic::Application.config.secret_token = 'e54ab8d92bc91a7428e553d5392be5a64e1e2d6b5d51790393bcf4175f2fd8ac1c340a78aae7d5a418fba48be4a5086d7eeeb5a5cf26cea6246ff272b21ff9fa'
