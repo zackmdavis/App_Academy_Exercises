474902160b56df0af767ee35f1ce1fc0f128bf77
Zack M. Davis <code@zackmdavis.net>
2013-10-16 13:33:26 -0700

    body params, too

--- Week_5/Wires/lib/rails_lite/params.rb
+++ Week_5/Wires/lib/rails_lite/params.rb
@@ -1,9 +1,13 @@
+require 'webrick'
 require 'uri'
 require 'json'
 
 class Params
   def initialize(request, route_params)
-    @params = request.query
+    body_string = request.body
+    query_string = request.query_string
+    param_string = [body_string, query_string].join('&')
+    @params = Params.parse_www_encoded_form(param_string)
   end
 
   def [](key)
@@ -14,17 +18,22 @@ class Params
     @params.to_json
   end
 
-  # private
-  # def parse_from_url(url)
-  #   query_string = url.split('?')[1]
-  #   params_array URI.decode_www_form(query_string)
-  #   @params = {}
-  #   @params = params_array.each do |k, v|
-  #     @params[k] = v
-  #   end
-  #   @params
-  # end
-  #
-  # def parse_key(key)
-  # end
+  def self.parse_www_encoded_form(query_style_string)
+    raw_hash = WEBrick::HTTPUtils.parse_query(query_style_string)
+    nesting_regexp = /\]\[|\[|\]/
+    simple_hash = raw_hash.reject { |k, _| nesting_regexp =~ k }
+    compound_hash =  raw_hash.select { |k, _| nesting_regexp =~ k }
+    leveled_hash = {}
+    compound_hash.each do |k, v|
+      keys = k.split(nesting_regexp)
+      level_hash = leveled_hash
+      keys[0..-2].each do |key|
+        level_hash[key] ||= {}
+        level_hash = level_hash[key]
+      end
+      level_hash[keys[-1]] = v
+    end
+    simple_hash.merge(leveled_hash)
+  end
+
 end
