bc335bad88d7531241120473fadbfbcd03445d87
Zack M. Davis <code@zackmdavis.net>
2013-10-24 12:12:24 -0700

    attempts click handling with Irene Zhou

--- Week_6/Hanoi/hanoi-ui.js
+++ Week_6/Hanoi/hanoi-ui.js
@@ -1,9 +1,14 @@
 var buildTowers = function() {
+  $('#peg1').remove();
+  $('#peg2').remove();
+  $('#peg3').remove();
   var towers = Game.towers;
+  $('body').append('<div class="peg" id ="peg1">');
+  $('body').append('<div class="peg" id ="peg2">');
+  $('body').append('<div class="peg" id ="peg3">');
   for (var i = 1; i <= 3; i++){
     for (var j = towers[i-1].length - 1; j>=0; j--) {
-      $('#peg'+i).append("<div id= 'disc" + j + "'>")
-      $('#peg'+i+' #disc' + j).addClass("disc"+towers[i-1][j]);
+      $('#peg'+i).append("<div class = 'disc" + towers[i-1][j] + "'>");
     }
   }
 }
@@ -11,16 +16,27 @@ var buildTowers = function() {
 
 $(document).ready(function(){
   buildTowers();
-  discs = [$('#disc1'), $('#disc2'), $('#disc3')]
-  for (var i=0; i<discs.length; i++){
-    discs[i].click(function(e){
-      alert("hi");
+  pegs = [$('#peg1'), $('#peg2'), $('#peg3')]
+  pegs[0].click(function(e) {
+    var startTowerIdx = 0;
+    console.log("start handler set");
+    pegs[1].click(function(){
+      var endTowerIdx = 1;
+      console.log("click");
+      Game.takeTurn(startTowerIdx, endTowerIdx);
+      buildTowers();
     });
 
-  }
-  // $('#disc').onClick(function(e){
-  //
-  // });
-
+  });
 
 });
+
+
+  // READER.question("Enter a starting tower: ",function (start) {
+  //   var startTowerIdx = parseInt(start);
+  //   READER.question("Enter an ending tower: ", function (end) {
+  //     var endTowerIdx = parseInt(end);
+  //     game.takeTurn(startTowerIdx,endTowerIdx);
+  //   });
+  // });
+  //};
