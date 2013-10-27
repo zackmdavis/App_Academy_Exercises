2c8edb56380ba53e827928b6ea874d81748d4fa6
Zack M. Davis <code@zackmdavis.net>
2013-10-27 13:37:53 -0700

    attempts assessment practice exercises

--- Week_6/a05-practice/src/js_assessment.js
+++ Week_6/a05-practice/src/js_assessment.js
@@ -1,15 +1,87 @@
 Assessment = {};
 
-Assessment.mergeSort = function(array) {};
+Assessment.mergeSort = function(array) {
+    var merge = function(left, right) {
+	var merged = [];
+	var merged_length = left.length + right.length;
+	while (merged.length < merged_length){
+	    if (left.length === 0){
+		merged = merged.concat(right);
+	    } else if (right.length === 0) {
+		merged = merged.concat(left);
+	    } else { 
+		if (left[0] < right[0]) {
+		    merged.push(left.shift());
+		} else {
+		    merged.push(right.shift());
+		}
+	    }
+	}
+	return merged;
+    }
 
-Assessment.recursiveExponent = function(base, power) {};
+    if (array.length <= 1) {
+	return array;
+    }
+    var center = array.length/2;
+    var left = Assessment.mergeSort(array.slice(0, center));
+    var right = Assessment.mergeSort(array.slice(center, array.length));
+    return merge(left, right);
+};
 
-Assessment.powCall = function (base, power, callback) {};
+Assessment.recursiveExponent = function(base, power) {
+    if (power === 0) {
+	return 1;
+    }
+    return base*Assessment.recursiveExponent(base, power-1);
+};
 
-Assessment.transpose = function (matrix) {};
+Assessment.powCall = function (base, power, callback) {
+    var result = Math.pow(base, power);
+    callback(result);
+    return result;
+};
 
-Assessment.primes = function (n) {};
+Assessment.transpose = function (matrix) {
+    var col = function(c) {
+	var column = [];
+	matrix.forEach(function(row) { column.push(row[c]) });
+	return column;
+    };
 
-Function.prototype.myApply = function (context, argArray) {};
+    var transposed = []
+    for(i = 0; i < matrix[0].length; i++) {
+	transposed.push(col(i));
+    }
+    return transposed;
 
-Function.prototype.inherits = function (Parent) {};
+};
+
+Assessment.primes = function (n) {
+    var the_primes = [];
+    var checking = 2;
+    while (the_primes.length < n){
+	var prime = true;
+	for (i = 2; i < checking; i++){
+	    if (checking % i === 0){
+		prime = false;
+		break;
+	    }
+	}
+	if (prime) {
+	    the_primes.push(checking);
+	}
+	checking++;
+    }
+    return the_primes;
+};
+
+Function.prototype.myCall = function (context, argArray) {
+    // actually not quite sure what to do here
+};
+
+Function.prototype.inherits = function (Parent) {
+    var Surrogate = function() {};
+    Surrogate.prototype = Parent.prototype;
+    this.prototype = new Surrogate();
+};
--- Week_6/a05-practice/src/wakeboard_game.js
+++ Week_6/a05-practice/src/wakeboard_game.js
@@ -1,1 +1,27 @@
 // WakeboardGame module
+
+// for some reason SpecRunner.html can't find my code here; what am I
+// doing wrong??
+(function(root){
+    var WakeboardGame = root.WakeboardGame = function() {};
+
+    var Boat = WakeboardGame.Boat = function(sponsor) {
+	this.sponsor = sponsor;
+    };
+
+    Boat.prototype.power = function() {return "power";}
+    Boat.prototype.turn = function() {return "turn";}
+    Boat.prototype.sink = function() {return "sink";}
+    
+    
+    var Wakeboarder = WakeboardGame.Wakeboarder(name, sponsor) {
+	this.name = name;
+	this.sponsor = sponsor;
+    };
+
+    Wakeboarder.prototype.jump = function() {return "jump";}
+    Wakeboarder.prototype.spin = function() {return "spin";}
+    Wakeboarder.prototype.grind = function() {return "grind";}
+    Wakeboarder.prototype.crash = function() {return "crash";}
+
+})(this);
