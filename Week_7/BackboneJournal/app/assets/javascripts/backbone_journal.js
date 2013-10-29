9a9c9ac665d98e9f3baf2a3083207f7b22fd068c
Zack M. Davis <code@zackmdavis.net>
2013-10-29 15:48:16 -0700

    start BackboneJournal project

---
+++ Week_7/BackboneJournal/app/assets/javascripts/backbone_journal.js
@@ -0,0 +1,13 @@
+window.BackboneJournal = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  BackboneJournal.initialize();
+});
---
+++ Week_7/BackboneJournal/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneJournal::Application.config.secret_token = 'b21b3e491c386a3cb32b3b6377fc376ff4d915f4359fd226f15c26eb4c098342bcc273552a548548892d817fe61f5105159f51625107fe40c14cd0a033cd72d6'
