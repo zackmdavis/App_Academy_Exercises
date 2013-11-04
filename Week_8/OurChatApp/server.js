c9efa825e2ba69a890d30bf2acf09d4344ede872
Zack M. Davis <code@zackmdavis.net>
2013-11-04 11:26:06 -0800

    server and form with Ben Watts

--- Week_8/OurChatApp/server.js
+++ Week_8/OurChatApp/server.js
@@ -2,10 +2,40 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
+// var router = require('./router.js');
 
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
+  var router = new Router(request, response);
+  router.process();
 }).listen(8080);
 
-console.log('Server running at http://127.0.0.1:8080/');
+
+var Router = function(req, res){
+  this.req = req;
+  this.res = res;
+};
+
+Router.prototype.process = function(){
+  var path = this.req.url;
+
+  var router = this;
+
+  var fileCallback = function(err, data){
+    if (err){
+      router.res.writeHead(404)
+      console.log(router.res.statusCode);
+      router.res.end();
+    } else {
+      router.res.writeHead(200, {'Content-Type': 'text/html'});
+      router.res.write(data);
+      router.res.end();
+    }
+  };
+
+  if (path === '/'){
+    fs.readFile('public/index.html', fileCallback);
+  } else {
+    fs.readFile(path, fileCallback);
+  }
+};
+
